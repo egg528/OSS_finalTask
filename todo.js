8503d86449e0e55843c56b650da6c712fde60a3c
egg528 <62459196+egg528@users.noreply.github.com>
2020-12-16 23:01:41 +0900

    할 일 추가하기 완성

--- todo.js
+++ todo.js
@@ -3,7 +3,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
   const TODOS_LS = "toDos";
-  const toDos = [];
+  let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -13,6 +24,7 @@ function saveToDos() {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     span.innerText = text;
